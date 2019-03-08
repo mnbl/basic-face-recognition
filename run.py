6e3b94d42b88776f92c47428edf0fddd5570cb87
mnbl <mandeepbaliyan@outlook.com>
2019-03-08 18:24:10 +0530

    face-detection

--- run.py
+++ run.py
@@ -1,30 +1,49 @@
-import numpy as np
 import cv2
+import sys
+from time import sleep
 
-cap = cv2.VideoCapture(0)
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
 
-while(True):
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-    
-    ret, frame = cap.read()
+video_capture = cv2.VideoCapture(0)
+anterior = 0
+
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    if anterior != len(faces):
+        anterior = len(faces)
+
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('Video', frame)
+
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
 
-cap.release()
+# When everything is done, release the capture
+video_capture.release()
 cv2.destroyAllWindows()
